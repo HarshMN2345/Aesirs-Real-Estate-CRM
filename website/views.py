de124a858dd90bc3a5e5f515c4a681f9482b1762
Harsh Mahajan <harshmahajan2345@gmail.com>
2023-08-13 17:55:37 +0530

    registeration

--- website/views.py
+++ website/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
-# Create your views here.
+def register_user(request):
+    return render(request, 'register.html')  
+def home(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        user=authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.info(request, ('You have been logged in!'))
+            return redirect('home')
+        else:
+            messages.info(request, ('Error logging in - Please try again...'))
+            return redirect('home')
+    return render(request, 'home.html')        
+def login_user(request):
+   return render(request, 'login.html')
+def logout_user(request):
+    logout(request)
+    messages.info(request, ('You have been logged out!'))
+    return redirect('home')
+   
